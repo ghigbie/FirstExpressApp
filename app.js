684db73f1f7a59d80d65c47037494a314f7b936c
ghigbie <gehigbie@gmail.com>
2017-02-10 23:27:45 +0000

    defined a function to send a request and to listen on a port in cloud 9

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 var express = require("express");
 var app = express();
 
+// "/" => "hi there!"  
+app.get("/", function(req, res){
+    res.send("Hi there!");
+    
+}); 
+// "/by" => "Goodbye"
+// "/dog" => "MEOW!"
+
+app.listen(process.env.PORT, process.env.IP);
+
+
