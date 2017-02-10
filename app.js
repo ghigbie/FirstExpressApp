24abee74d798705ae8904df6bb09c2611558ade4
ghigbie <gehigbie@gmail.com>
2017-02-10 23:34:06 +0000

    add new server response with html

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var app = express();
 // "/" => "hi there!"  
 app.get("/", function(req, res){
     res.send("Hi there!");
-    
 }); 
-// "/by" => "Goodbye"
+// "/bye" => "Goodbye"
+
+app.get("/bye", function(req, res){
+    res.send("<h1>Goodbye</h1>");
+});
 // "/dog" => "MEOW!"
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started. Yay!!!");
+});
 
 
